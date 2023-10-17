cbf8defe6f0e79379efca27c12eeb778039443af
Joshua Hecker <jhecker2001@gmail.com>
2023-10-17 09:39:44 -0500

    Created logic for making bids between one or more bidders.

--- blind_auction/blind_auction.py
+++ blind_auction/blind_auction.py
@@ -5,3 +5,16 @@ def clear():
     os.system('cls')
 
 print(logo)
+
+print("Welcome to the secret auction program.")
+bidders = {}
+bidding = True
+
+while bidding:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bidders[name] = bid
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if more_bidders != "yes":
+        bidding = False
+    clear()
